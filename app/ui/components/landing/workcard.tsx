df286a204c589135872149292718ea55d560c8df
anjmhrjn <anjmhrjn1@gmail.com>
2024-10-23 10:19:36 -0400

    first page responsiveness

--- app/ui/components/landing/workcard.tsx
+++ app/ui/components/landing/workcard.tsx
@@ -14,25 +14,27 @@ export default function WorkCard({
   return (
     <div
       key={`work-${index}`}
-      className={`mt-16 rounded-xl py-6 px-16 drop-shadow-xl min-h-[458px] max-h-[458px] overflow-hidden flex space-x-6 align-middle items-center h-full`}
+      className={`mt-16 rounded-xl py-6 px-8 lg:px-16 drop-shadow-xl min-h-[458px] overflow-hidden flex md:space-x-6 align-middle items-center h-full`}
       style={{ backgroundColor: work.bgColor }}
     >
       <div className="flex-1">
+        <div className="mb-4">
         {work.tags.map((tag, tIndex) => (
           <span
             key={`work-${index}-tag-${tIndex}`}
-            className={`${mulish.className} inline-block text-sm px-3 py-2 rounded-lg mb-4 mr-4`}
+            className={`${mulish.className} inline-block text-sm px-3 py-2 rounded-lg mb-2 mr-4`}
             style={{ backgroundColor: tag.bgColor, color: tag.color }}
           >
             {tag.tagName}
           </span>
         ))}
+        </div>
 
         <div className=" mb-2">
           {work.label.map((label, lIndex) => (
             <h2
               key={`work-${index}-label-${lIndex}`}
-              className="text-4xl dark-blue-text"
+              className="text-3xl lg:text-4xl dark-blue-text"
             >
               {label}
             </h2>
@@ -54,7 +56,7 @@ export default function WorkCard({
       </div>
 
       {/* Right Section - Image */}
-      <div className="flex-shrink-0">
+      <div className="flex-shrink-0 hidden md:block">
         <Image src={`${work.logo}`} className="object-contain" alt="Logo" width={250} height={250} />
       </div>
     </div>
